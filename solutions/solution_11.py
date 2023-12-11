6f12a3f72cb4b14494924a73200daa07fcbfb1f6
Raine M. Ekman <raine@iki.fi>
2023-12-11 22:55:47 +0000

    Cleaned up solution 11, updated requirements.txt.

--- solutions/solution_11.py
+++ solutions/solution_11.py
@@ -10,13 +10,14 @@ def solve_11(data):
     # Expand the universe...
     universe = np.char.array(data)
     # ...by inserting empty lines     
-    inserted = 0
+    inserted = 0 # This adjusts the cursor to keep track...
     for i, galaxycount in enumerate(universe.count('#')):
         if galaxycount == 0:
                universe = np.insert(universe, i+inserted, '.' * len(universe[i]))
                inserted += 1
     
-    # ...and columns
+    # ...and columns. Not sure rotating was a good idea, but it makes
+    # for elegant(-ish) finding of '#' characters
     universe = np.rot90(universe.view('U1').reshape(universe.size,-1))  
     inserted = 0
     for i, line in enumerate(universe):
@@ -36,54 +37,42 @@ def solve_11(data):
          for j in range(i+1,len(positions)):
             # Manhattan distance
             distances[i][j] = abs(positions[i][0] - positions[j][0]) + abs(positions[i][1] - positions[j][1])
-                    
 
-    print(positions)
-    print(distances)
-
-    print(universe)
-    print(universe.shape)
     return sum(sum(distances))
 
 # As the first part, but now expand empty rows/columns by one million...
+# so it has to be through indexes instead
 def solve_11b(data, factor=1000000):    
     # Expand the universe...
     universe = np.char.array(data)
-    # ...by inserting empty lines     
-    inserted = 0
-    for i, galaxycount in enumerate(universe.count('#')):
-        if galaxycount == 0:
-               universe = np.insert(universe, i+inserted, '.' * len(universe[i]))
-               inserted += 1
-    
-    # ...and columns
-    universe = np.rot90(universe.view('U1').reshape(universe.size,-1))  
-    inserted = 0
-    for i, line in enumerate(universe):
+
+    rownum = [] # To hold the renumbering of rows
+    r = 0
+    for galaxycount in universe.count('#'):
+        rownum.append(r)
+        r += factor if galaxycount == 0 else 1
+
+    colnum = []
+    c = 0
+    universe = np.rot90(universe.view('U1').reshape(universe.size,-1))      
+    for line in universe:
+        colnum.append(c)
         chr,counts = np.unique(line, return_counts=True)
         counts = dict(zip(chr, counts))
-        if not '#' in counts:             
-             # The axis thing because we now have a 2D array of char...
-             universe = np.insert(universe,i+inserted, line, axis=0)
-             inserted += 1
+        c += factor if not '#' in counts else 1             
 
     tmp = np.where(universe == '#')
-    positions = list(zip(tmp[0],tmp[1]))
+    pos = list(zip(tmp[0],tmp[1]))
 
-    distances = np.zeros((len(positions),len(positions)),dtype=np.int32)
+    distances = np.zeros((len(pos),len(pos)))
     # quadratic time, but is there any other way?
-    for i in range(0,len(positions)):
-         for j in range(i+1,len(positions)):
+    for i in range(0,len(pos)):
+         for j in range(i+1,len(pos)):
             # Manhattan distance
-            distances[i][j] = abs(positions[i][0] - positions[j][0]) + abs(positions[i][1] - positions[j][1])
-                    
-
-    print(positions)
-    print(distances)
-
-    print(universe)
-    print(universe.shape)
-    return sum(sum(distances))
+            distances[i][j] = abs(colnum[pos[i][0]] - colnum[pos[j][0]]) + \
+                abs(rownum[pos[i][1]] - rownum[pos[j][1]])
+    
+    return int(sum(sum(distances)))
 
 
 if __name__ == '__main__':
